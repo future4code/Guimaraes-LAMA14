b506865dd380e8708418d4cd28d457c276bd1fce
Ana Carolina Kazue Inada <kazu@MacBook-Air-de-Ana.local>
2022-09-08 21:00:05 -0300

    template com o cadastro feito

---
+++ src/business/UserBusiness.ts
@@ -0,0 +1,53 @@
+import { UserInputDTO, LoginInputDTO, User } from "../model/User";
+import { UserDatabase } from "../data/UserDatabase";
+import { IdGenerator } from "../services/IdGenerator";
+import { HashManager } from "../services/HashManager";
+import { Authenticator } from "../services/Authenticator";
+
+export class UserBusiness {
+  createUser = async (user: UserInputDTO) => {
+    const idGenerator = new IdGenerator();
+    const id = idGenerator.generate();
+
+    const hashManager = new HashManager();
+    const hashPassword = await hashManager.hash(user.password);
+
+    const userDatabase = new UserDatabase();
+    const newUser = new User(
+      id,
+      user.name,
+      user.email,
+      hashPassword,
+      user.role
+    );
+    await userDatabase.createUser(newUser);
+
+    const authenticator = new Authenticator();
+    const accessToken = authenticator.generate({ id, role: user.role });
+
+    return accessToken;
+  };
+
+  async getUserByEmail(user: LoginInputDTO) {
+    const userDatabase = new UserDatabase();
+    const userFromDB = await userDatabase.getUserByEmail(user.email);
+
+    const hashManager = new HashManager();
+    const hashCompare = await hashManager.compare(
+      user.password,
+      userFromDB.getPassword()
+    );
+
+    const authenticator = new Authenticator();
+    const accessToken = authenticator.generate({
+      id: userFromDB.getId(),
+      role: userFromDB.getRole(),
+    });
+
+    if (!hashCompare) {
+      throw new Error("Invalid Password!");
+    }
+
+    return accessToken;
+  }
+}
---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,23 @@
+import { BaseDatabase } from "./BaseDatabase";
+import { User } from "../model/User";
+
+export class UserDatabase extends BaseDatabase {
+  private static TABLE_NAME = "User_lama";
+
+  public createUser = async (user: User): Promise<void> => {
+    try {
+      await UserDatabase.connection(UserDatabase.TABLE_NAME).insert(user);
+    } catch (error: any) {
+      throw new Error(error.sqlMessage || error.message);
+    }
+  };
+
+  public async getUserByEmail(email: string): Promise<User> {
+    const result = await UserDatabase.connection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ email });
+
+    return User.toUserModel(result[0]);
+  }
+}
